f444d60dea8c569c39d7d0a1df5cb9458e4dcffe
Lunev Oleg <lunev2k@outlook.com>
2018-03-15 08:45:36 +0700

    Change format Date and Time

--- app/src/main/java/com/lunev2k/schedule/utils/DateTimeUtil.java
+++ app/src/main/java/com/lunev2k/schedule/utils/DateTimeUtil.java
@@ -1,6 +1,5 @@
 package com.lunev2k.schedule.utils;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -11,12 +10,13 @@ public class DateTimeUtil {
     }
 
     public static String getFormatDate(Date date) {
-        return DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault()).format(date);
+        SimpleDateFormat outputformat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        return outputformat.format(date);
     }
 
     public static String getFormatTime(Date time) {
-        DateFormat outputformat = new SimpleDateFormat("HH:mm");
-        return DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault()).format(time);
+        SimpleDateFormat outputformat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        return outputformat.format(time);
     }
 
     public static String getFormatDateTime(Date datetime) {
